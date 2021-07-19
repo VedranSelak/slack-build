b72578cbad5b06750f4d17916e04725bfd4f1f3b
VedranSelak <selakvedran@gmail.com>
2021-07-19 14:28:47 +0200

    cleaned up code and installed react-router-dom

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPodBDr-Lfe8sYC9vABK5co5FeD3nQwsc",
+    authDomain: "slack-build-f853a.firebaseapp.com",
+    projectId: "slack-build-f853a",
+    storageBucket: "slack-build-f853a.appspot.com",
+    messagingSenderId: "521912389977",
+    appId: "1:521912389977:web:a623371db1662124cf3c48",
+    measurementId: "G-RBXV4KQE60"
+  };
+
+  const firebaseApp = firebase.initializeApp();
